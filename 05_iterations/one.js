e6c8769103184ac281d84664b558067143f4290f
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2024-06-09 10:18:58 +0000

    updated

---
+++ 05_iterations/one.js
@@ -0,0 +1,58 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// AGAR LOOPS NESTED NA HO TOH HAM SAME VARIABLE USE KAR SAKTE HAI
+// LIKE IN THIS CASE i
+
+// BUT IF THE LOOPS ARE NESTED THEN WE HAVE TO USE DIFFERENT VARIABLES
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop: ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner loop: ${j} and Outer loop: ${i}`);
+//     }
+// }
+
+// let myArray = ['flash','batman','superman']
+// console.log(myArray.length);
+// for (let index = 0; index <= myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// LAST VALUE undefined AAYI KYOKI JAVASCRIPT MEIN OUT OF RANGE JAISA KOI ERROR NAHI HOTA HAI
+
+// SOMETIMES WE HAVE TO STOP THE ITERATION BASED ON A CONDITION
+
+// break AND continue
+
+// for (let index = 1; index <= 20; index++) {
+//     if(index==5){
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`value of index is ${index}`);
+// }
+
+// continue KA AISA HAI KI EK BAAR MAAF KARDO
+// JUST LIKE IN GAMES, KI AGAR FOUL HO GAYA TOH EK BAAR MAAF KARDO
+
+// for (let index = 1; index < 10; index++) {
+//     if(index==6){
+//         console.log(`Detected 6 but you can continue`);
+//         continue
+//     }
+//     console.log(`value of index is ${index}`);
+// }
+
+
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,26 @@
+// while AND do while LOOP
+
+// let index = 0 
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2 // UPDATION OF INDEX IS VERY IMPORTANT BECAUSE IT CAN LEAD TO INFINITE LOOP IF NOT WRITTEN
+// }
+
+// while LOOP IN ARRAY
+
+// let myArray = ['batman','superman','flash']
+// let arr = 0
+// while (arr<myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++ // arr++ => arr = arr + 1
+// }                                             
+
+
+// DO WHILE LOOP => YAHA KAAM PEHLE HOTA HAI AUR CONDITION BAAD MEIN CHECK HOTI HAI
+// TOH ISILIYE 11 PRINT HO GAYA
+
+let score = 11
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<=10);
