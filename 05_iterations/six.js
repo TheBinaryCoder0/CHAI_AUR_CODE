374cad736894c1a82198cb022b2de92607c12c44
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2024-06-10 20:38:03 +0000

    updated

---
+++ 05_iterations/six.js
@@ -0,0 +1,17 @@
+// const coding = ["js","ruby","java","python","cpp"]
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item // forEach DOES NOT RETURN ANY VALUE
+// } )
+
+// console.log(values);
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// filter VALUES RETURN KARTA HAI
+const newNums = myNums.filter( (num) => num > 4 )
+// const newNums = myNums.filter( (num) => {
+//    return num > 4 } )
+
+console.log(newNums); 
