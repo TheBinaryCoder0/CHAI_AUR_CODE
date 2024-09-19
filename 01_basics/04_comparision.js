0b904b8babde940637dc155ca6a7418cde1ad8b0
Aman Shahi <118298784+TheBinaryCoder0@users.noreply.github.com>
2024-09-19 15:37:43 +0000

    updated

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,17 +1,21 @@
-// console.log(2>1);  => true 
-// console.log(2>=1); => true
-// console.log(2<1); => false
-// console.log(2==1); => false
-// console.log(2!=1); => true
+console.log(2>1);  // => true 
+console.log(2>=1); // => true
+console.log(2<1);  // => false
+console.log(2==1); // => false
+console.log(2!=1); // => true
+
+console.log();
 
 // PROBLEMS OCCUR WHEN WE DON'T COMPARE THE SAME DATA TYPES
 
-// console.log("2">1);  // JAVASCRIPT AUTOMATICALLY CONVERTS THE DATATYPES BUT IT IS NOT ALLOWED IN TYPESCRIPT
-// console.log("02">1);
+console.log("2">1);  // JAVASCRIPT AUTOMATICALLY CONVERTS THE DATATYPES BUT IT IS NOT ALLOWED IN TYPESCRIPT
+console.log("02">1); // // FOR RELATIONAL COMPARISIONS, JS CONVERTS STRINGS INTO NUMBERS, NOT NUMBERS INTO STRINGS
 
-// console.log(null>0);
-// console.log(null==0);
-// console.log(null>=0);
+console.log();6666666666666
+
+console.log(null>0); // => false
+console.log(null==0); // => false
+console.log(null>=0); // => true
 
 /*
 THE REASON IS THAT AN EQUALITY CHECK == AND COMPARISIONS > < >= <= WORKS DIFFERENTLY.
@@ -19,10 +23,13 @@ COMPARISIONS CONVERT NULL TO A NUMBER, TREATING IT AS 0.
 THAT'S WHY null>=0 IS true AND null>0 IS false
 */
 
+console.log();
 
-// console.log(undefined == 0);
-// console.log(undefined>0);
-// console.log(undefined<0);
+console.log(undefined == 0); // => false
+console.log(undefined>0); // => false
+console.log(undefined<0); // => false
+
+console.log();
 
 /*
 IN ALL THE ABOVE CASES THE OUTPUT WILL BE false
